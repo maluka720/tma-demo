690fe5989a6950553a9aa019e55c3f16e501ca98
majun <majun@teamind.co>
2023-11-29 23:30:37 +0800

    add pay

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 import WebApp from "@twa-dev/sdk";
 import "./App.css";
 
@@ -8,9 +8,19 @@ function App() {
       <h2 style={{ color: "#0088cc" }}>Smart Trade</h2>
       {/* Here we add our button with alert callback */}
       <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! `)}>充值</button>
-        <button onClick={() => WebApp.showAlert(`coming soon `)}>下单</button>
+        <button
+          style={{ backgroundColor: "#0088cc", color: "#fff" }}
+          onClick={() => WebApp.showAlert(`Hello World! `)}
+        >
+          充值
+        </button>
       </div>
+      <div className="card">
+        <button onClick={() => WebApp.showAlert(`coming soon `)}>赞助</button>
+      </div>
+      {/* <div className="card">
+        <button onClick={() => WebApp.showAlert(`coming soon `)}>下单</button>
+      </div> */}
     </>
   );
 }
