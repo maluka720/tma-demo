f21d74b902489c64bddd35128a4ded07e3a12bc9
majun <majun@teamind.co>
2023-11-29 23:17:40 +0800

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,15 @@
 import { useState } from "react";
 import WebApp from "@twa-dev/sdk";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2 style={{ color: "#0088cc" }}>Smart Trade</h2>
       {/* Here we add our button with alert callback */}
       <div className="card">
-        <button
-          onClick={() =>
-            WebApp.showAlert(`Hello World! Current count is ${count}`)
-          }
-        >
-          Show Alert
-        </button>
+        <button onClick={() => WebApp.showAlert(`Hello World! `)}>充值</button>
+        <button onClick={() => WebApp.showAlert(`coming soon `)}>下单</button>
       </div>
     </>
   );
